52a20c56e85a0814f6a97a41a93426c357d5e774
Scumsy <dmitry.koschavka@gmail.com>
2022-08-20 23:35:18 +0300

    task 2 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function () {});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+// save current time -----------------------
+const saveTime = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(Math.floor(data.seconds))
+  );
+};
+player.on('timeupdate', _.throttle(saveTime, 1000));
+
+// set current time from memory ------------------
+const currentTimeFromMemory = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(currentTimeFromMemory)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
